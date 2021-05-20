f67775a3bb5758e782f8ecd4f5701debc58c2414
Peter Kosarnik <nestastnikos@github.com>
2021-05-20 20:39:44 +0200

    change request interface of API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import { http } from '@/http'
 import { ApiResult, SparqlResult } from './types'
 
-export async function apiRequest(endpoint: string, query: any): Promise<ApiResult> {
-  const response = await http.get(endpoint, {
+export async function apiRequest(endpoint: string, query: any, suffix: string = ''): Promise<ApiResult> {
+  const response = await http.get(endpoint + suffix, {
     params: {
       format: 'json',
       origin: '*',
@@ -14,17 +14,17 @@ export async function apiRequest(endpoint: string, query: any): Promise<ApiResul
 }
 
 export async function sparqlRequest(endpoint: string, query: string): Promise<SparqlResult> {
-  // const response = await http.get(endpoint, {
-  //   params: {
-  //     format: 'json',
-  //     query,
-  //   },
-  // })
+  const response = await http.get(endpoint, {
+    params: {
+      format: 'json',
+      query,
+    },
+  })
 
-  const params = new URLSearchParams()
-  params.append('format', 'json')
-  params.append('query', query)
-  const response = await http.post(endpoint, params)
+  // const params = new URLSearchParams()
+  // params.append('format', 'json')
+  // params.append('query', query)
+  // const response = await http.post(endpoint, params)
 
   return response.data
 }
